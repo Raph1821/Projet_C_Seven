8f871d88516f2fb074084027da7c3ff1df2cbca3
NGUYEN_Minh_Nhut <Minh_Nhut.Nguyen@etu.sorbonne-universite.fr>
2025-04-01 17:40:04 +0200

    Explication du code Generic_card_parser.hpp

--- code_skeleton/Generic_card_parser.hpp
+++ code_skeleton/Generic_card_parser.hpp
@@ -1,17 +1,17 @@
-#pragma once
+#pragma once // une directive préprocesseur qui permet d'éviter l'inclusion multiple d'un fichier d'en-tête(.h/.hpp)
 
 #include <string>
-#include <unordered_map>
+#include <unordered_map> 
 #include <iostream>
 
-namespace sevens {
+namespace sevens {  // Éviter les conflits de noms + Organiser le code + Rendre le code plus lisible 
 
 /**
  * Represents a single playing card for Sevens:
  *   suit: 0..3  (0=Clubs,1=Diamonds,2=Hearts,3=Spades)
  *   rank: 1..13 (1=Ace,2=2,...,13=King)
  */
-struct Card {
+struct Card {   // Fonctionne comme une classe, la différence ici c'est que les membres de struct sont publics par défaut, types de données simples 
     int suit;
     int rank;
 
@@ -34,7 +34,7 @@ public:
         // unordered_map : une structure de donnée qui stocke des paires clé-valeur --> accès rapide grâce à une fonction de hachage 
         // uint64_t :  une clé de type entier non signé 64 bits 
         // Card : type de la valeur associée à chaque clé --> une struct ou une classe 
-        // & get_cards_hashmap() : retourne une référence vers une tablede hachage stockée dans l'objet 
+        // & get_cards_hashmap() : retourne une référence vers une table de hachage stockée dans l'objet 
         return this->cards_hashmap;
     }
 
