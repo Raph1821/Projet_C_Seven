f795437fc8466c86b228b3aa2059ca68f1d61b8c
NGUYEN_Minh_Nhut <Minh_Nhut.Nguyen@etu.sorbonne-universite.fr>
2025-04-24 11:07:07 +0200

    Explication Generic_game_mapper.hpp

--- code_skeleton/Generic_game_mapper.hpp
+++ code_skeleton/Generic_game_mapper.hpp
@@ -16,23 +16,20 @@ namespace sevens {
  */
 class Generic_game_mapper : public Generic_game_parser {
 public:
-    virtual std::vector<std::pair<uint64_t, uint64_t>>
-    compute_game_progress(uint64_t numPlayers) = 0;
+    virtual std::vector<std::pair<uint64_t, uint64_t>>compute_game_progress(uint64_t numPlayers) = 0; // std::pair --> un conteneur qui contient deux valeurs de types possiblement différents.
 
-    virtual std::vector<std::pair<uint64_t, uint64_t>>
-    compute_and_display_game(uint64_t numPlayers) = 0;
+    virtual std::vector<std::pair<uint64_t, uint64_t>>compute_and_display_game(uint64_t numPlayers) = 0;
 
     // Overloads if you want name-based players
-    virtual std::vector<std::pair<std::string, uint64_t>>
-    compute_game_progress(const std::vector<std::string>& playerNames) {
-        (void)playerNames; // avoid unused-parameter warning
-        throw("Not implemented");
+    virtual std::vector<std::pair<std::string, uint64_t>>compute_game_progress(const std::vector<std::string>& playerNames) {
+    // std::string --> nom du joueur , uint_t --> un score ou un état de jeu : {"Alice",5} 
+        (void)playerNames; // avoid unused-parameter warning (dit que la variable 'playerNames' n'est pas utilisée --> je sais que je ne l'utilise pas,c'est normal)
+        throw("Not implemented"); // pour dire que cette fonction n'a pas encore été codée --> je t'autorise à redéfinir cette méthode dans une classe fille, mais par défaut elle ne fait rien (et te prévient si tu l'appelles par erreur)
     }
 
-    virtual std::vector<std::pair<std::string, uint64_t>>
-    compute_and_display_game(const std::vector<std::string>& playerNames) {
-        (void)playerNames;
-        throw("Not implemented");
+    virtual std::vector<std::pair<std::string, uint64_t>>compute_and_display_game(const std::vector<std::string>& playerNames) {
+        (void)playerNames; // même logique
+        throw("Not implemented"); // même logique 
     }
 };
 
